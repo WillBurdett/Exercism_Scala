cca2e438b8118541cc60f9b6674bb773fb4ed1a8
William Burdett <williamburdett@williams-air.mynet>
2022-04-17 13:50:49 +0100

    'ArmstrongNumbers' solution refactored

--- armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -1,13 +1,8 @@
 
-
  object ArmstrongNumbers {
     def isArmstrongNumber(num: Int): Boolean = {
-      var totalDigits = num.toString.split("").length
-      var sum: Double = 0
-      for (e<-num.toString.split("")) {
-        sum = sum + math.pow(e.toDouble, totalDigits)
-      }
-      if (sum == num.toDouble) true else false
+      var numArr = num.toString.split("")
+      if (numArr.map(e => math.pow(e.toDouble, (numArr.length))).sum == num.toDouble) true else false
     }
   }
 
