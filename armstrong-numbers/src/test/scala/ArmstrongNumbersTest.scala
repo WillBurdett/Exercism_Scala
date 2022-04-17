c7209bf02dd3e284792838d7441a48c7e057708b
William Burdett <williamburdett@williams-air.mynet>
2022-04-17 13:33:34 +0100

    all tests passes for ArmstrongNumbers

--- armstrong-numbers/src/test/scala/ArmstrongNumbersTest.scala
+++ armstrong-numbers/src/test/scala/ArmstrongNumbersTest.scala
@@ -22,22 +22,18 @@ class ArmstrongNumbersTest extends AnyFunSuite with Matchers {
   }
 
   test("Four digit number that is an Armstrong number") {
-    pending
     ArmstrongNumbers.isArmstrongNumber(9474) should be (true)
   }
 
   test("Four digit number that is not an Armstrong number") {
-    pending
     ArmstrongNumbers.isArmstrongNumber(9475) should be (false)
   }
 
   test("Seven digit number that is an Armstrong number") {
-    pending
     ArmstrongNumbers.isArmstrongNumber(9926315) should be (true)
   }
 
   test("Seven digit number that is not an Armstrong number") {
-    pending
     ArmstrongNumbers.isArmstrongNumber(9926314) should be (false)
   }
 }
