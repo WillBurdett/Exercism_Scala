487bcbdca7e5f9c5648f5dc8148f6dfa73761a44
William Burdett <williamburdett@williams-air.mynet>
2022-04-20 14:29:48 +0100

    'Hamming' all tests complete

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -1,7 +1,12 @@
 
 object Hamming extends App {
-  def distance(a: String, b: String): Some[Int] = {
 
+  // in functional programming 'null' returns are avoided
+  // so what do we do instead? We use Option/Some/None
+  def distance(a: String, b: String): Option[Int] = {
+    if (a.trim.length != b.trim.length) {
+      return None
+    }
     var totalDifferences: Int = 0;
     for (i <- a.split("").indices) if (a.split("")(i) != b.split("")(i)) totalDifferences+=1
     Some(totalDifferences)
