223c42bb51f826205e12d28d8cd8611f9b560e55
William Burdett <williamburdett@williams-air.mynet>
2022-04-20 14:42:15 +0100

    User solution added (used zip function)

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -12,3 +12,15 @@ object Hamming extends App {
     Some(totalDifferences)
   }
 }
+
+  // aaylward's solution!
+  object Hamming2 {
+    def distance2(first: String, second: String): Option[Int] = {
+      if (first.length != second.length) {
+        return None
+      }
+      // zip creates a tuple out of first and second.
+      // p._1 is referring to the first first element of the tuple - not a partial function!
+      Some(first zip second count (p => p._1 != p._2))
+    }
+  }
