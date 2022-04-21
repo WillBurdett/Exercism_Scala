97e9adf1246422f960f71f729ed2c671ce08d707
William Burdett <williamburdett@Williams-MacBook-Air.local>
2022-04-21 14:17:42 +0100

    'Etl' all tests completed

--- etl/src/main/scala/Etl.scala
+++ etl/src/main/scala/Etl.scala
@@ -7,11 +7,17 @@ object Etl extends App {
 
     // iterate over maps getting keys and values
     for((k, v) <- legacyMap){
-      println(v)
+
+        // iterate over the Seq[String] found in the Map for each key(Int)
+        // indices provides the index
         for (letter <- v.indices){
+          // append the result to the existing map
+          // WARNING: this will replace any value with the same key
+          // However, that did not matter in this instance
           result += (v(letter).toString.toLowerCase -> k)
         }
       }
+    // return the Map each of the new key/value pairs have been added
     result
   }
 }
