85aff814ce8b3283923483986394647eaa8b8581
William Burdett <williamburdett@williams-air.mynet>
2022-04-25 09:28:31 +0100

    Input sorted to double

--- triangle/src/main/scala/Triangle.scala
+++ triangle/src/main/scala/Triangle.scala
@@ -1,5 +1,15 @@
-case class Triangle (a: Any, b: Any, c: Any){
-  def equilateral(): Boolean = if (a == b && b == c && a != 0) true else false
-  def isosceles(): Boolean = if ((a == b && b == c && a != 0) || (a == b || b == c | a == c)) true else false
-  def scalene(): Boolean = if (a == b && b == c) false else true
+case class Triangle(a: Double, b: Double, c: Double) {
+
+  // Set must all be unique, and so this calculates how many sides match/are unique
+  val uniqueSides = (Set(a, b, c) - 0).size
+
+  // valid triangle if :
+  // - sum of each two sides > one side remaining
+  // - Sides cannot all be 0
+  def valid: Boolean = a + b > c && b + c > a && c + a > b
+
+  def equilateral: Boolean = valid && uniqueSides == 1
+  // the '<' accomodates more incase it is '1' ad therefore equilateral
+  def isosceles: Boolean = valid && uniqueSides <= 2
+  def scalene: Boolean = valid && uniqueSides == 3
 }
