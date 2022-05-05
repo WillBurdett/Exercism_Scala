09bfaf67bff748487f0a84c3a82a4cfb7b16270b
William Burdett <williamburdett@Williams-MacBook-Air.local>
2022-05-05 11:53:01 +0100

    CollatzConjecture passing 2/6 tests

---
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -0,0 +1,16 @@
+import scala.annotation.tailrec
+
+object CollatzConjecture{
+  def steps(num: Int): Option[Int] = {
+
+    @tailrec
+    def calculator(calcInput: Int, calcIteration: Int): Option[Int] = {
+      calcInput match {
+        case 1 => Some(calcIteration)
+        case _ => if (isEven(calcInput)) calculator(calcInput / 2, calcIteration + 1) else calculator((calcInput * 3) + 1, calcIteration + 1)
+      }
+    }
+    calculator(num, 0)
+  }
+  def isEven(num: Int): Boolean = if (num % 2 == 0) true else false
+}
